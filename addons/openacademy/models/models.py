465d4ea7dd1a7e5af90733a42952df8f7801d163
KeyData Developer <aruizna@bd360.online>
2019-12-11 13:24:25 -0300

    Practica Many2one

--- addons/openacademy/models/models.py
+++ addons/openacademy/models/models.py
@@ -32,7 +32,7 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
-    #see_course = fields.Many2many('openacademy.course','name')
+    see_course = fields.Many2many('openacademy.course','name')
     instructor_id = fields.Many2one('res.partner', string="Instructor", domain="[('instructor','=',True)]")
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
